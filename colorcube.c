b909877d9dcb5b7228fa4230ec3579fa49374310
prathikshagk <96339287+prathikshagk@users.noreply.github.com>
2022-07-25 22:12:05 +0530

    Update colorcube.c

--- colorcube.c
+++ colorcube.c
@@ -100,7 +100,7 @@ int main(int argc,char **argv)
      glutInit(&argc,argv);
      glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
      glutInitWindowSize(500,500);
-     glutCreateWindow("color cube and spin using camera_4MT19CS115");
+     glutCreateWindow("color cube and spin using camera");
      glutReshapeFunc(myreshape);
      glutDisplayFunc(display);
      glutMouseFunc(mouse);
